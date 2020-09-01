f31328ec158a94bf42ba50485308e3fc29c1db63
Orion <orionji@icloud.com>
2020-09-01 20:40:22 +0800

    Create ex38.py

---
+++ ex38/ex38.py
@@ -0,0 +1,21 @@
+ten_thing = "Apple Organge Crows Telephone Light Sugar"
+
+print("Wait there is not 10 things in that list. Let's fix that.")
+
+stuff = ten_thing.split(' ')
+more_stuff = ["Day","Night","Song","Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+
+while len(stuff) != 10 :
+	next_one = more_stuff.pop()
+	print("Adding:", next_one)
+	stuff.append(next_one)
+	print(f"There are {len(stuff)} items now")
+
+print("There we go:", stuff)
+
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
